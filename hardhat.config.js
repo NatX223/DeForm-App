cfd45ae3dff10c58804a4bac97cd249df2a835ea
sinachi ajuzie <Ajuziesinachi@gmail.com>
2023-09-04 12:58:00 +0100

    complete Project setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+import { PRIVATE_KEY } from "./credentials.mjs";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    hardhat: {
+    },
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/x66ngpknfweaoFrjMzdrIio3F3-YqUDV",
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
